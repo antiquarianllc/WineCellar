ab1979170007c9c9911957673f8b5a384c1a3175
Rich Bietz <richbietz@antiquariansoftware.com>
2020-10-21 10:41:53 -0700

    Add migration to create WineVarietal database table.

---
+++ sources/WineCellar/Migrations/20201021034913_CreateWineVarietalTable.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+namespace WineCellar.Migrations
+{
+    public partial class CreateWineVarietalTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Varietals",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Varietal = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Varietals", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Varietals");
+        }
+    }
+}
