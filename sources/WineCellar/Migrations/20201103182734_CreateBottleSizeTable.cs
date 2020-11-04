e0b48c8f79d29abce33f46bc5d92db0da333cbbf
Rich Bietz <richbietz@antiquariansoftware.com>
2020-11-04 09:50:45 -0800

    Migrations used to create bottle size table and to initialize it.

---
+++ sources/WineCellar/Migrations/20201103182734_CreateBottleSizeTable.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+namespace WineCellar.Migrations
+{
+    public partial class CreateBottleSizeTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "BottleSizes",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    BottleSize = table.Column<string>(nullable: true),
+                    Volume = table.Column<string>(nullable: true),
+                    Default = table.Column<bool>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_BottleSizes", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "BottleSizes");
+        }
+    }
+}
