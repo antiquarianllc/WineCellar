9f66e91dfc1242befe02bb43a6a7341e10d79bec
Rich Bietz <richbietz@antiquariansoftware.com>
2020-11-04 09:48:48 -0800

    Create model and entity objects related bottle sizes.

---
+++ sources/WineCellar/Helpers/BottleSizeConverter.cs
@@ -0,0 +1,46 @@
+
+
+
+namespace WineCellar.Helpers
+{
+    using WineCellar.Entities;
+    using WineCellar.Models;
+
+    public class BottleSizeConverter
+    {
+        private WineCellarDBContext _context;
+
+        public BottleSizeConverter( WineCellarDBContext context ) => _context = context;
+
+        public BottleSizeEntity Convert( BottleSizeModel source )
+        {
+            BottleSizeEntity destination = _context.BottleSizes.Find( source.Id );
+
+            if (destination == null)
+            {
+                destination = new BottleSizeEntity( );
+            }
+
+            destination.Id = source.Id;
+            destination.BottleSize = source.Size;
+            destination.Volume = source.Volume;
+
+            return destination;
+        }
+
+        public BottleSizeModel Convert( BottleSizeEntity source )
+        {
+
+            BottleSizeModel destination = new BottleSizeModel( )
+            {
+                Id = source.Id,
+                Size = source.BottleSize,
+                Volume = source.Volume,
+                Default = source.Default
+        };
+
+            return destination;
+        }
+    }
+}
+
