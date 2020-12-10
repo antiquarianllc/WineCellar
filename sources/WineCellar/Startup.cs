a8691dc81baf40038c33179d81358ca65e6a84c2
Rich Bietz <richbietz@antiquariansoftware.com>
2020-12-10 08:36:08 -0800

    Add Swashbuckle/Swagger service. This allows auto-generation of API documentation.

--- sources/WineCellar/Startup.cs
+++ sources/WineCellar/Startup.cs
@@ -11,6 +11,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
 
 namespace WineCellar
 {
@@ -38,6 +39,12 @@ namespace WineCellar
             services.AddDbContext<WineCellarDBContext>( opt => opt.UseNpgsql( builder.ConnectionString ) );
 
             services.AddControllers();
+
+            // 2020/12/10 - Add Swashbuckle service to auto-generate API documentation.
+            services.AddSwaggerGen( options =>
+            {
+                options.SwaggerDoc( "v1", new OpenApiInfo { Title = "WineCellar API", Version = "v1" } );
+            } );
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -46,22 +53,29 @@ namespace WineCellar
             // 2020/10/21 - Move applying migrations to only happen in 'Developement' environment.
             //--wcConext.Database.Migrate( );
 
-            if (env.IsDevelopment())
+            if ( env.IsDevelopment( ) )
             {
                 wcConext.Database.Migrate( );
-                app.UseDeveloperExceptionPage();
+                app.UseDeveloperExceptionPage( );
             }
 
-            app.UseHttpsRedirection();
+            app.UseHttpsRedirection( );
 
-            app.UseRouting();
+            app.UseRouting( );
 
-            app.UseAuthorization();
+            app.UseAuthorization( );
 
-            app.UseEndpoints(endpoints =>
+            // 2020/12/10 - Add Swashbuckle service to auto-generate API documentation.
+            app.UseSwagger( );
+            app.UseSwaggerUI( options => 
             {
-                endpoints.MapControllers();
-            });
+                options.SwaggerEndpoint( "/swagger/v1/swagger.json", "WineCellar API v1" );
+            } );
+
+            app.UseEndpoints( endpoints =>
+            {
+                 endpoints.MapControllers( );
+            } );
         }
     }
 }
